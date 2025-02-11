f0b9c7013cd15959a587662a3c51df86c8181b5d
Abhijeet_15 <118336271+AbhijeetKamalekar15@users.noreply.github.com>
2025-02-11 22:57:19 +0530

    ........

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,17 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+
 import { env } from "@/env";
 
 const createPrismaClient = () =>
   new PrismaClient({
-    log: env.NODE_ENV === "development" ? ["warn", "error"] : ["error"],
+    log:
+      env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
   });
 
 const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient;
+  prisma: ReturnType<typeof createPrismaClient> | undefined;
 };
 
-const db = globalForPrisma.prisma ?? createPrismaClient();
+export const db = globalForPrisma.prisma ?? createPrismaClient();
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
-
-export { db };
