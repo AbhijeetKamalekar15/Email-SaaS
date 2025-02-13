a92c34463e7b56981422997cd7d45ca454710be2
Abhijeet_15 <118336271+AbhijeetKamalekar15@users.noreply.github.com>
2025-02-13 17:49:51 +0530

    Aurinko setup done!!!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
+import LinkAccountButton from "@/components/link-account-button";
 import { Button } from "@/components/ui/button";
 
 
 export default async function Home() {
-  return <Button>Click me</Button>
+  return <LinkAccountButton/>
 }
